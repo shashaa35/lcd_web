7ce4a12d6cafe50ea235965c3dd24596639c5d28
Shashank AGarwal <shashaa35@gmail.com>
2018-10-20 07:21:34 +0000

    adding rspec test cases

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,35 +1,31 @@
-#
-# Cookbook:: lcd_web
-# Spec:: default
-#
-# Copyright:: 2018, The Authors, All Rights Reserved.
-
 require 'spec_helper'
 
 describe 'lcd_web::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'CentOS' do
     let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.2.1511')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
-    end
-  end
+    end    
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it 'installs httpd' do
+      expect(chef_run).to install_package('httpd')
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'installs net-tools' do
+      expect(chef_run).to install_package('net-tools')
     end
+
+    it 'enables the httpd service' do
+      expect(chef_run).to enable_service('httpd')
+    end
+
+    it 'starts the httpd service' do
+      expect(chef_run).to start_service('httpd')
+    end
+
   end
 end
